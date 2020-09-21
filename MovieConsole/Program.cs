d04b4c1b69cc5848ee5fe4792fdff74108c0bcd5
p-karol <priscilla.karolczak@gmail.com>
2020-09-21 02:00:15 +0100

    editando apresentacao para usar fachada

--- MovieConsole/Program.cs
+++ MovieConsole/Program.cs
@@ -1,10 +1,13 @@
-﻿using Entidades.Model;
+﻿using Dominio;
+using Entidades.Model;
+using Entidades.ViewModel;
 using Microsoft.EntityFrameworkCore;
 
 using Persistencia.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MovieConsole
 {
@@ -13,6 +16,24 @@ namespace MovieConsole
 
         static void Main(String[] args)
         {
+
+            static async Task Main(string[] args)
+            {
+                FachadaDominio fachada = new FachadaDominio();
+
+                var relat = await fachada.getRelatorioGeneros();
+
+                Console.WriteLine("Relatorio - generos");
+
+                foreach (GenreSummary gen in relat.OrderByDescending(g => g.Faturamento))
+                {
+                    Console.WriteLine("\nGenero: {0}", gen.Categoria);
+                    Console.WriteLine("\tFaturamento total: {0:C}\n\tAvaliação média: {1,6:N2}\n\tNumero de filmes: {2,2}",
+                                    gen.Faturamento, gen.Avaliacao, gen.Quantidade);
+                }
+            }
+
+
             MovieContext db = new MovieContext();
 
             #region # LINQ - consultas
