c8d4aa0f5f74a22cc60b78eb82e4115ee616e163
p-karol <priscilla.karolczak@gmail.com>
2020-09-26 19:40:57 +0100

    Criadas classes modelo Actor e ActorMovie e alterado contexto

---
+++ Entidades/Model/ActorMovie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entidades.Model
+{
+    public class ActorMovie
+    {
+        public int ActorMovieId { get; set; }
+        public string Character { get; set; }
+        public int MovieId { get; set; }
+        public int ActorId { get; set; }
+    }
+}
