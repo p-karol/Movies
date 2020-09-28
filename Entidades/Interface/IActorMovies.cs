e1112dc34015bd0c18f0ae1523fc0de7edb84c5b
p-karol <priscilla.karolczak@gmail.com>
2020-09-28 01:13:36 +0100

    Correcoes para funcionar o migration

---
+++ Entidades/Interface/IActorMovies.cs
@@ -0,0 +1,21 @@
+﻿using Entidades.Model;
+using Entidades.ViewModel;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Interface
+{
+    public interface IGenres
+    {
+        Task Inserir(Genre genre);
+        Task<List<Genre>> List();
+        Task Delete(Genre genre);
+        Task<Genre> GetGenreById(int Id);
+        Task Update(Genre genre);
+
+        Task<ICollection<GenreSummary>> getGenreSummary();
+    }
+}
